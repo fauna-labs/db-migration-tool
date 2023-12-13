50941b98adff1d412d0d0e747f3665e26f7d86a2
Paul Paterson <ptpaterson@gmail.com>
2023-12-13 15:44:56 -0500

    Add and run prettier

--- functions.js
+++ functions.js
@@ -32,159 +32,165 @@ const {
 function getEventsFromCollectionFunctionQuery(name) {
   return Let(
     {
-      name: name
+      name: name,
     },
-    If(Exists(Function(Var("name"))),
+    If(
+      Exists(Function(Var("name"))),
       true,
       CreateFunction({
         name: name,
-        body:
-          Query(
-            Lambda(
-              ["startTime", "indexName", "interval", "size", "after", "before"],
-              Let(
-                {
-                  match: Match(Index(Var("indexName"))),
-                  endTime: ToMicros(
-                    TimeAdd(
-                      Epoch(Var("startTime"), "microseconds"),
-                      Var("interval"),
-                      "minutes"
-                    )
+        body: Query(
+          Lambda(
+            ["startTime", "indexName", "interval", "size", "after", "before"],
+            Let(
+              {
+                match: Match(Index(Var("indexName"))),
+                endTime: ToMicros(
+                  TimeAdd(
+                    Epoch(Var("startTime"), "microseconds"),
+                    Var("interval"),
+                    "minutes",
                   ),
-                  page: If(
-                    Equals(Var("before"), null),
-                    If(
-                      Equals(Var("after"), null),
-                      Paginate(Range(Var("match"), Var("startTime"), []), {
-                        size: Var("size")
-                      }),
-                      Paginate(Range(Var("match"), Var("startTime"), []), {
-                        after: Var("after"),
-                        size: Var("size")
-                      })
-                    ),
+                ),
+                page: If(
+                  Equals(Var("before"), null),
+                  If(
+                    Equals(Var("after"), null),
                     Paginate(Range(Var("match"), Var("startTime"), []), {
-                      before: Var("before"),
-                      size: Var("size")
-                    })
+                      size: Var("size"),
+                    }),
+                    Paginate(Range(Var("match"), Var("startTime"), []), {
+                      after: Var("after"),
+                      size: Var("size"),
+                    }),
                   ),
-                  refs: Map(Var("page"), Lambda(["t", "r"], Var("r"))),
-                  eventList: Map(
-                    Var("refs"),
-                    Lambda("e", Paginate(Events(Var("e")), { size: 100000 }))
-                  ),
-                  recent: Map(
-                    Var("eventList"),
-                    Lambda(
-                      "el",
-                      Filter(
-                        Var("el"),
-                        Lambda(
-                          "t",
-                          And(
-                            GTE(Select("ts", Var("t")), Var("startTime")),
-                            LT(Select("ts", Var("t")), Var("endTime"))
-                          )
-                        )
-                      )
-                    )
-                  ),
-                  filtered: Filter(Var("recent"), Lambda("el", IsNonEmpty(Var("el"))))
-                },
-                Map(
-                  Var("filtered"),
+                  Paginate(Range(Var("match"), Var("startTime"), []), {
+                    before: Var("before"),
+                    size: Var("size"),
+                  }),
+                ),
+                refs: Map(Var("page"), Lambda(["t", "r"], Var("r"))),
+                eventList: Map(
+                  Var("refs"),
+                  Lambda("e", Paginate(Events(Var("e")), { size: 100000 })),
+                ),
+                recent: Map(
+                  Var("eventList"),
                   Lambda(
-                    "res",
-                    Map(
-                      Var("res"),
-                      Lambda("event", {
-                        ts: Select("ts", Var("event")),
-                        doc: Select("document", Var("event")),
-                        action: Select("action", Var("event")),
-                        data: Select("data", Var("event"), null)
-                      })
-                    )
-                  )
-                )
-              )
-            )
-          )
-      })
-    )
+                    "el",
+                    Filter(
+                      Var("el"),
+                      Lambda(
+                        "t",
+                        And(
+                          GTE(Select("ts", Var("t")), Var("startTime")),
+                          LT(Select("ts", Var("t")), Var("endTime")),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                filtered: Filter(
+                  Var("recent"),
+                  Lambda("el", IsNonEmpty(Var("el"))),
+                ),
+              },
+              Map(
+                Var("filtered"),
+                Lambda(
+                  "res",
+                  Map(
+                    Var("res"),
+                    Lambda("event", {
+                      ts: Select("ts", Var("event")),
+                      doc: Select("document", Var("event")),
+                      action: Select("action", Var("event")),
+                      data: Select("data", Var("event"), null),
+                    }),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      }),
+    ),
   );
 }
 
 function getRemoveEventsFromCollectionFunctionQuery(name) {
   return Let(
     {
-      name: name
+      name: name,
     },
-    If(Exists(Function(Var("name"))),
+    If(
+      Exists(Function(Var("name"))),
       true,
       CreateFunction({
         name: name,
-        body:
-          Query(
-            Lambda(
-              ["startTime", "collection", "interval", "size", "after", "before"],
-              Let(
-                {
-                  match: Documents(Collection(Var("collection"))),
-                  endTime: ToMicros(
-                    TimeAdd(
-                      Epoch(Var("startTime"), "microseconds"),
-                      Var("interval"),
-                      "minutes"
-                    )
+        body: Query(
+          Lambda(
+            ["startTime", "collection", "interval", "size", "after", "before"],
+            Let(
+              {
+                match: Documents(Collection(Var("collection"))),
+                endTime: ToMicros(
+                  TimeAdd(
+                    Epoch(Var("startTime"), "microseconds"),
+                    Var("interval"),
+                    "minutes",
                   ),
-                  page: If(
-                    Equals(Var("before"), null),
-                    If(
-                      Equals(Var("after"), null),
-                      Paginate(Var("match"), {
-                        after: { ts: Var("startTime") },
-                        size: Var("size"),
-                        events: true
-                      }),
-                      Paginate(Var("match"), {
-                        after: Var("after"),
-                        size: Var("size"),
-                        events: true
-                      })
-                    ),
+                ),
+                page: If(
+                  Equals(Var("before"), null),
+                  If(
+                    Equals(Var("after"), null),
                     Paginate(Var("match"), {
-                      before: Var("before"),
+                      after: { ts: Var("startTime") },
                       size: Var("size"),
-                      events: true
-                    })
+                      events: true,
+                    }),
+                    Paginate(Var("match"), {
+                      after: Var("after"),
+                      size: Var("size"),
+                      events: true,
+                    }),
                   ),
-                  removeEvents: Filter(
-                    Var("page"),
-                    Lambda(
-                      "e",
-                      And(
-                        Equals(Select(["action"], Var("e")), "remove"),
-                        GTE(Select("ts", Var("e")), Var("startTime")),
-                        LT(Select("ts", Var("e")), Var("endTime"))
-                      )
-                    )
-                  )
-                },
-                Map(
-                  Var("removeEvents"),
-                  Lambda("e", {
-                    ts: Select("ts", Var("e")),
-                    doc: Select("document", Var("e")),
-                    action: Select("action", Var("e"))
-                  })
-                )
-              )
-            )
-          )
-      })
-    )
+                  Paginate(Var("match"), {
+                    before: Var("before"),
+                    size: Var("size"),
+                    events: true,
+                  }),
+                ),
+                removeEvents: Filter(
+                  Var("page"),
+                  Lambda(
+                    "e",
+                    And(
+                      Equals(Select(["action"], Var("e")), "remove"),
+                      GTE(Select("ts", Var("e")), Var("startTime")),
+                      LT(Select("ts", Var("e")), Var("endTime")),
+                    ),
+                  ),
+                ),
+              },
+              Map(
+                Var("removeEvents"),
+                Lambda("e", {
+                  ts: Select("ts", Var("e")),
+                  doc: Select("document", Var("e")),
+                  action: Select("action", Var("e")),
+                }),
+              ),
+            ),
+          ),
+        ),
+      }),
+    ),
   );
 }
 
-module.exports = { getEventsFromCollectionFunctionQuery, getRemoveEventsFromCollectionFunctionQuery }
+module.exports = {
+  getEventsFromCollectionFunctionQuery,
+  getRemoveEventsFromCollectionFunctionQuery,
+};
--- main.js
+++ main.js
@@ -5,24 +5,43 @@ const { program } = require("commander");
 
 (async () => {
   // Start the program - first time
-  // Provide the startTime, collection, index and how many minutes of writes the destination db needs to catch up each time. 
+  // Provide the startTime, collection, index and how many minutes of writes the destination db needs to catch up each time.
   // Number of iterations are configurable
-  // With duration=30 and iteration=10, it migrates  30 mins worth of data from source db and does this 10 times, 
-  // waiting 2 mins (interval=120) between each fetch 
+  // With duration=30 and iteration=10, it migrates  30 mins worth of data from source db and does this 10 times,
+  // waiting 2 mins (interval=120) between each fetch
   program
     .name("fauna-db-sync")
-    .description('migrates lastest writes from one DB to another')
-    .version('0.0.0', '-v, --version')
-    .usage('[OPTIONS]...')
-    .requiredOption('-s, --source <string>', 'admin secret for the source DB')
-    .requiredOption('-t, --target <string>', 'admin secret for the target DB')
-    .requiredOption('-c, --collection <string>', 'the name of the collection to be sync\'ed')
-    .requiredOption('-d, --timestamp <number>', 'the timestamp from which to start syncing', parseInt)
-    .option('-i, --index <string>', '[optional] the name of the index to use to sync the collection')
-    .option('-p, --parallelism <number>', '[optional] apply up to N events per transaction', parseParallelism, 10)
-    .option('--validate <number>', '[optional] paginate through documents N at a time (1000 max) and compare source to target; WARNING: this could take a long time and will accrue additional read ops', parseInt)
+    .description("migrates lastest writes from one DB to another")
+    .version("0.0.0", "-v, --version")
+    .usage("[OPTIONS]...")
+    .requiredOption("-s, --source <string>", "admin secret for the source DB")
+    .requiredOption("-t, --target <string>", "admin secret for the target DB")
+    .requiredOption(
+      "-c, --collection <string>",
+      "the name of the collection to be sync'ed",
+    )
+    .requiredOption(
+      "-d, --timestamp <number>",
+      "the timestamp from which to start syncing",
+      parseInt,
+    )
+    .option(
+      "-i, --index <string>",
+      "[optional] the name of the index to use to sync the collection",
+    )
+    .option(
+      "-p, --parallelism <number>",
+      "[optional] apply up to N events per transaction",
+      parseParallelism,
+      10,
+    )
+    .option(
+      "--validate <number>",
+      "[optional] paginate through documents N at a time (1000 max) and compare source to target; WARNING: this could take a long time and will accrue additional read ops",
+      parseInt,
+    )
     .parse(process.argv);
-  
+
   const options = program.opts();
 
   if (options.validate) {
@@ -40,29 +59,45 @@ const { program } = require("commander");
     lastProcessed.updates.ts = options.timestamp;
     lastProcessed.removes.ts = options.timestamp;
 
-    console.log(`BEGIN synchronizing events in collection '${options.collection}' at ${new Date().toISOString()}`);
+    console.log(
+      `BEGIN synchronizing events in collection '${
+        options.collection
+      }' at ${new Date().toISOString()}`,
+    );
 
-    let initialized = await initialize(options.collection, index, options.source, options.target)
+    let initialized = await initialize(
+      options.collection,
+      index,
+      options.source,
+      options.target,
+    );
     if (!initialized) {
-      throw new Error("Initialization error; can't continue")
+      throw new Error("Initialization error; can't continue");
     }
 
     do {
-      await migrate(options.collection, index, duration, size, options.parallelism);
+      await migrate(
+        options.collection,
+        index,
+        duration,
+        size,
+        options.parallelism,
+      );
 
       lastProcessed.startTime += duration * 60 * 1000 * 1000; //increase the start time by 'duration' amount of minutes at every iteration
 
       await pause(interval * 1000).then(
-        console.log(`Sleeping for ${interval} seconds`)
+        console.log(`Sleeping for ${interval} seconds`),
       ); //sleep in ms
 
       iterations--;
-    } while (
-      iterations > 0 &&
-      lastProcessed.startTime < Date.now() * 1000
-    )
+    } while (iterations > 0 && lastProcessed.startTime < Date.now() * 1000);
 
-    console.log(`END synchronizing events in collection '${options.collection}' at ${new Date().toISOString()}`);
+    console.log(
+      `END synchronizing events in collection '${
+        options.collection
+      }' at ${new Date().toISOString()}`,
+    );
   }
 })().catch((err) => {
   console.error(err);
