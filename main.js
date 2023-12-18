a04dcdac000255a9c906a0ba4069817a6ea67279
Paul Paterson <ptpaterson@gmail.com>
2023-12-18 15:50:54 -0500

    Go back to IIFE

--- main.js
+++ main.js
@@ -11,49 +11,47 @@ const ITERATIONS = 20; // Number of iterations to run the tool
 const WAIT_TIME = 10; // Wait time between iterations in seconds
 const DEFAULT_PAGE_SIZE = 64; // Page size for retrieving documents from the custom index
 
-program
-  .name("fauna-db-sync")
-  .description("migrates lastest writes from one DB to another")
-  .version("0.0.0", "-v, --version")
-  .usage("[OPTIONS]...")
-  .requiredOption("-s, --source <string>", "admin secret for the source DB")
-  .requiredOption("-t, --target <string>", "admin secret for the target DB")
-  .requiredOption(
-    "-d, --timestamp <number>",
-    "the timestamp from which to start syncing",
-    parseInt,
-  )
-  .option(
-    "-c, --collections <string...>",
-    "[optional] the list of Collection names to be sync'ed",
-  )
-  .option(
-    "-i, --indexes <string...>",
-    "[optional] the list of Index names to be used with the respective Collections listed",
-  )
-  .option(
-    "-p, --parallelism <number>",
-    "[optional] apply up to N events per transaction",
-    parseParallelism,
-    10,
-  )
-  .option(
-    "--validate <number>",
-    "[optional] paginate through documents N at a time (1000 max) and compare source to target; WARNING: this could take a long time and will accrue additional read ops",
-    parseInt,
-  )
-  .parse(process.argv);
-
-const options = program.opts();
-
-run(options);
-
-async function run(options) {
+(async () => {
   // Start the program - first time
   // Provide the startTime, collection, index and how many minutes of writes the destination db needs to catch up each time.
   // Number of iterations are configurable
   // With DURATION=30 and ITERATIONS=10, it migrates 30 mins worth of data from source db and does this 10 times,
   // waiting WAIT_TIME seconds between each iteration.
+
+  program
+    .name("fauna-db-sync")
+    .description("migrates lastest writes from one DB to another")
+    .version("0.0.0", "-v, --version")
+    .usage("[OPTIONS]...")
+    .requiredOption("-s, --source <string>", "admin secret for the source DB")
+    .requiredOption("-t, --target <string>", "admin secret for the target DB")
+    .requiredOption(
+      "-d, --timestamp <number>",
+      "the timestamp from which to start syncing",
+      parseInt,
+    )
+    .option(
+      "-c, --collections <string...>",
+      "[optional] the list of Collection names to be sync'ed",
+    )
+    .option(
+      "-i, --indexes <string...>",
+      "[optional] the list of Index names to be used with the respective Collections listed",
+    )
+    .option(
+      "-p, --parallelism <number>",
+      "[optional] apply up to N events per transaction",
+      parseParallelism,
+      10,
+    )
+    .option(
+      "--validate <number>",
+      "[optional] paginate through documents N at a time (1000 max) and compare source to target; WARNING: this could take a long time and will accrue additional read ops",
+      parseInt,
+    )
+    .parse(process.argv);
+
+  const options = program.opts();
 
   const migrator = new MigrationClient({
     sourceKey: options.source,
@@ -115,85 +113,83 @@ async function run(options) {
   }
   console.log();
 
-  try {
-    if (options.validate) {
-      console.log("Ready to Validate collections.");
+  if (options.validate) {
+    console.log("Ready to Validate collections.");
 
-      for (const collection of collectionList) {
-        const { collectionName, indexName } = collection;
+    for (const collection of collectionList) {
+      const { collectionName, indexName } = collection;
 
-        console.log(`Validating migration of Collection("${collectionName}")`);
+      console.log(`Validating migration of Collection("${collectionName}")`);
 
-        const validationOptions = {
-          ...options,
-          collection: collectionName,
-          index: indexName,
-        };
+      const validationOptions = {
+        ...options,
+        collection: collectionName,
+        index: indexName,
+      };
 
-        await validate(validationOptions);
-      }
-    } else {
-      console.log("Ready to Synchronize collections.");
+      await validate(validationOptions);
+    }
+  } else {
+    console.log("Ready to Synchronize collections.");
 
-      console.log("Initializing UDFs...");
-      await migrator.initializeSourceFunctions();
+    console.log("Initializing UDFs...");
+    await migrator.initializeSourceFunctions();
+    console.log();
+
+    console.log(`Initializing Collections...`);
+    let collectionsInitialized = true;
+    for (const collection of collectionList) {
+      const { collectionName, indexName } = collection;
+      const initialized = await migrator.initializeCollection({
+        collectionName,
+        indexName,
+      });
+      collectionsInitialized = collectionsInitialized && initialized;
+    }
+    if (!collectionsInitialized) {
+      throw new Error("Initialization error; can't continue");
+    }
+    console.log();
+
+    let iterations = ITERATIONS;
+    let startTime = options.timestamp;
+
+    console.log(`BEGIN synchronizing events at ${new Date().toISOString()}`);
+    console.log();
+
+    do {
+      const startString = new Date(startTime / 1000).toISOString();
+      const endString = new Date(
+        startTime / 1000 + DURATION * 60 * 1000,
+      ).toISOString();
+      console.log(
+        `Searching for events between ${startString} and ${endString}`,
+      );
       console.log();
 
-      console.log(`Initializing Collections...`);
-      let collectionsInitialized = true;
       for (const collection of collectionList) {
         const { collectionName, indexName } = collection;
-        const initialized = await migrator.initializeCollection({
+
+        console.log(`Synchronizing Collection("${collectionName}")`);
+
+        await migrator.migrateCollection({
           collectionName,
           indexName,
+          startTime,
+          duration: DURATION,
         });
-        collectionsInitialized = collectionsInitialized && initialized;
-      }
-      if (!collectionsInitialized) {
-        throw new Error("Initialization error; can't continue");
-      }
-      console.log();
 
-      let iterations = ITERATIONS;
-      let startTime = options.timestamp;
-
-      console.log(`BEGIN synchronizing events at ${new Date().toISOString()}`);
-      console.log();
-
-      do {
-        const startString = new Date(startTime / 1000).toISOString();
-        const endString = new Date(
-          startTime / 1000 + DURATION * 60 * 1000,
-        ).toISOString();
-        console.log(
-          `Searching for events between ${startString} and ${endString}`,
-        );
+        console.log(`Sleeping for ${WAIT_TIME} seconds`);
+        await pause(WAIT_TIME * 1000); //sleep in ms
         console.log();
+      }
 
-        for (const collection of collectionList) {
-          const { collectionName, indexName } = collection;
+      startTime += DURATION * 60 * 1000 * 1000; //increase the start time by 'DURATION' amount of minutes at every iteration
+      iterations--;
+    } while (iterations > 0 && startTime < Date.now() * 1000);
 
-          console.log(`Synchronizing Collection("${collectionName}")`);
-
-          await migrator.migrateCollection({
-            collectionName,
-            indexName,
-            startTime,
-            duration: DURATION,
-          });
-
-          console.log(`Sleeping for ${WAIT_TIME} seconds`);
-          await pause(WAIT_TIME * 1000); //sleep in ms
-          console.log();
-        }
-
-        startTime += DURATION * 60 * 1000 * 1000; //increase the start time by 'DURATION' amount of minutes at every iteration
-        iterations--;
-      } while (iterations > 0 && startTime < Date.now() * 1000);
-
-      console.log(`END synchronizing events at ${new Date().toISOString()}`);
-    }
-  } catch (err) {
-    console.error(err);
+    console.log(`END synchronizing events at ${new Date().toISOString()}`);
   }
-}
+})().catch((err) => {
+  console.error(err);
+});
